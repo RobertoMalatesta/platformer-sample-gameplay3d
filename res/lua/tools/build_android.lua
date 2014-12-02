a8f635d1fca5133863e30f79badaec4f9a2b651d
louis-mclaughlin <louis.mclaughlin@yahoo.co.uk>
2014-12-02 23:30:28 +0000

    Automated android scripts now require that paths to android tools and ndk root be set in game.config, lua's os.execute doesn't seem to evaluate commands that rely on PATH

--- res/lua/tools/build_android.lua
+++ res/lua/tools/build_android.lua
@@ -1,2 +1,2 @@
-os.execute("android update project -t 1 -p ../android -s")
-os.execute("ndk-build -C ../android -j " ..  Game.getInstance():getConfig():getInt("debug_num_android_compile_jobs"))
+os.execute(Game.getInstance():getConfig():getString("debug_android_sdk_tools_dir") .. "/" .. "android update project -t 1 -p ../android -s")
+os.execute(Game.getInstance():getConfig():getString("debug_android_ndk_dir") .. "/" ..  "ndk-build -C ../android -j " ..  Game.getInstance():getConfig():getInt("debug_num_android_compile_jobs"))
