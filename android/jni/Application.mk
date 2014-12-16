9e4dab34e3dba83dd463bfc257eb4c3ab23bcb31
louis-mclaughlin <louis.mclaughlin@yahoo.co.uk>
2014-12-16 15:58:35 +0000

    Upgraded to gameplay 3.0

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
-APP_PLATFORM := android-10
+APP_PLATFORM := android-16
 APP_STL := gnustl_static
-APP_ABI := armeabi-v7a
+APP_ABI      := armeabi-v7a x86
 APP_CPPFLAGS += -frtti -std=gnu++11
-NDK_TOOLCHAIN_VERSION := clang
+NDK_TOOLCHAIN_VERSION := 4.8
