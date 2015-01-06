4ad46b72e17af8677c3c9b50623907f87e997e68
louis-mclaughlin <louis.mclaughlin@yahoo.co.uk>
2015-01-06 22:07:31 +0000

    Updated to latest gameplay drop, includes changes to external deps, lua lib no longer supports io operations, breaks tools

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -17,13 +17,7 @@
 #
 
 SAMPLE_PATH := $(call my-dir)/../../src
-PNG_PATH := ../external/GamePlay/external-deps/png/lib/android/$(TARGET_ARCH_ABI)
-ZLIB_PATH := ../external/GamePlay/external-deps/zlib/lib/android/$(TARGET_ARCH_ABI)
-LUA_PATH := ../external/GamePlay/external-deps/lua/lib/android/$(TARGET_ARCH_ABI)
-BULLET_PATH := ../external/GamePlay/external-deps/bullet/lib/android/$(TARGET_ARCH_ABI)
-OGG_PATH := ../external/GamePlay/external-deps/ogg/lib/android/$(TARGET_ARCH_ABI)
-VORBIS_PATH := ../external/GamePlay/external-deps/vorbis/lib/android/$(TARGET_ARCH_ABI)
-OPENAL_PATH := ../external/GamePlay/external-deps/openal/lib/android/$(TARGET_ARCH_ABI)
+GAMEPLAY_DEPS := ../external/GamePlay/external-deps/lib/android/$(TARGET_ARCH_ABI)
 
 # gameplay
 LOCAL_PATH := ../external/GamePlay/gameplay/android/obj/local/$(TARGET_ARCH_ABI)
@@ -39,60 +33,11 @@ LOCAL_MODULE    := libgameobjects
 LOCAL_SRC_FILES := libgameobjects.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-# libpng
-LOCAL_PATH := $(PNG_PATH)
+# libgameplay-deps
+LOCAL_PATH := $(GAMEPLAY_DEPS)
 include $(CLEAR_VARS)
-LOCAL_MODULE    := libpng
-LOCAL_SRC_FILES := libpng.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libz
-LOCAL_PATH := $(ZLIB_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libz
-LOCAL_SRC_FILES := libz.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# liblua
-LOCAL_PATH := $(LUA_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := liblua
-LOCAL_SRC_FILES := liblua.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libBulletCollision
-LOCAL_PATH := $(BULLET_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libBulletCollision
-LOCAL_SRC_FILES := libBulletCollision.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libBulletDynamics
-LOCAL_PATH := $(BULLET_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libBulletDynamics
-LOCAL_SRC_FILES := libBulletDynamics.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libogg
-LOCAL_PATH := $(OGG_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libogg
-LOCAL_SRC_FILES := libogg.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libvorbis
-LOCAL_PATH := $(VORBIS_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libvorbis
-LOCAL_SRC_FILES := libvorbis.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# libOpenAL
-LOCAL_PATH := $(OPENAL_PATH)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libOpenAL
-LOCAL_SRC_FILES := libOpenAL.a
+LOCAL_MODULE    := libgameplay-deps
+LOCAL_SRC_FILES := libgameplay-deps.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 # platformer
@@ -126,7 +71,7 @@ LOCAL_SRC_FILES :=  ../external/GamePlay/gameplay/src/gameplay-main-android.cpp 
 LOCAL_CPPFLAGS += -std=c++11 -Wno-switch-enum -Wno-switch
 LOCAL_ARM_MODE := arm
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -lOpenSLES
-LOCAL_CFLAGS    := -D__ANDROID__ -I"../external/GamePlay/external-deps/lua/include" -I"../external/GamePlay/external-deps/bullet/include" -I"../external/GamePlay/external-deps/png/include" -I"../external/GamePlay/external-deps/vorbis/include" -I"../external/GamePlay/external-deps/ogg/include" -I"../external/GamePlay/external-deps/openal/include" -I"../external/GamePlay/gameplay/src" -I"../external/gameobjects-gameplay3d/src"
+LOCAL_CFLAGS    := -D__ANDROID__ -I"../external/GamePlay/external-deps/include" -I"../external/GamePlay/gameplay/src" -I"../external/gameobjects-gameplay3d/src"
 
 LOCAL_STATIC_LIBRARIES := android_native_app_glue libgameplay libgameobjects libpng libz liblua libBulletDynamics libBulletCollision libvorbis libOpenAL
 
