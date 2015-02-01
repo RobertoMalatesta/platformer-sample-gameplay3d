c9dbc8d6eb09236dfe2a50eb945e670cca09a505
louis-mclaughlin <louis.mclaughlin@yahoo.co.uk>
2015-02-01 20:06:18 +0000

    Fixed bug where character physics was disabled when hand of god reset occurs while climbing a ladder

--- src/PlayerComponent.cpp
+++ src/PlayerComponent.cpp
@@ -350,7 +350,8 @@ namespace platformer
     {
         _state = State::Idle;
         gameplay::PhysicsCharacter * character = static_cast<gameplay::PhysicsCharacter*>(_characterNode->getCollisionObject());
-        character->resetVelocityState();
         _characterNode->setTranslation(gameplay::Vector3(position.x, position.y, 0));
+        character->resetVelocityState();
+        character->setPhysicsEnabled(true);
     }
 }
